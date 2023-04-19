55602ac694075d223cee58c5658b075062fe1d0c
marcelo7pro <marcelo@7promotora.com>
2023-04-19 16:15:57 -0300

    Projeto acessando API GIT

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -1,85 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
 
 import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
 
-import { Container, Sidebar, Main } from './styles';
+import { Loading, Container, Sidebar, Main } from './styles';
 
-import { getLangsFrom } from '../../services/api';
+import { getUser, getRepos, getLangsFrom } from '../../services/api';
 
 const RepositoriesPage = () => {
+  const { login } = useParams();
+
+  const [user, setUser] = useState();
+  const [repositories, setRepositories] = useState();
+  const [languages, setLanguages] = useState();
   const [currentLanguage, setCurrentLanguage] = useState();
+  const [loading, setLoading] = useState(true);
 
-  const user = {
-    login: 'devsamurai',
-    name: 'Felipe',
-    avatar_url: 'https://avatars.githubusercontent.com/u/55540536?v=4',
-    followers: 0,
-    following: 0,
-    company: null,
-    blog: 'https://devsamurai.com.br',
-    location: 'São José dos Campos - SP',
-  };
+  useEffect(() => {
+    const loadData = async () => {
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser(login),
+        getRepos(login),
+      ]);
 
-  // eslint-disable-next-line no-unused-vars
-  const repositories = [
-    {
-      id: '1',
-      name: 'Repo 1',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'JavaScript',
-    },
-    {
-      id: '2',
-      name: 'Repo 2',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'JavaScript',
-    },
-    {
-      id: '3',
-      name: 'Repo 3',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'PHP',
-    },
-    {
-      id: '4',
-      name: 'Repo 4',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: null,
-    },
-    {
-      id: '5',
-      name: 'Repo 5',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'TypeScript',
-    },
-    {
-      id: '6',
-      name: 'Repo 6',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'Ruby',
-    },
-    {
-      id: '7',
-      name: 'Repo 7',
-      description: 'Descrição',
-      html_url: 'https://devsamurai.com.br',
-      language: 'PHP',
-    },
-  ];
+      setUser(userResponse.data);
+      setRepositories(repositoriesResponse.data);
+      setLanguages(getLangsFrom(repositoriesResponse.data));
 
-  const languages = getLangsFrom(repositories);
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
 
   const onFilterClick = (language) => {
     setCurrentLanguage(language);
   };
+
+  if (loading) {
+    return <Loading>Carregando...</Loading>;
+  }
 
   return (
     <Container>
