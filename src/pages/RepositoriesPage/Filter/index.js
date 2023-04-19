992eab2a7889c7b091552d6d7a6be2f43d81c928
marcelo7pro <marcelo@7promotora.com>
2023-04-19 10:12:33 -0300

    Filtros funcionando

--- src/pages/RepositoriesPage/Filter/index.js
+++ src/pages/RepositoriesPage/Filter/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Selector, Cleaner } from './styles';
 
-const Filter = () => {
-  const langs = [
-    { name: 'JavaScript', count: 5, color: '#f1c40f' },
-    { name: 'Shell', count: 2, color: '#95a5a6' },
-    { name: 'PHP', count: 1, color: '#3498db' },
-  ];
-
-  const selectors = langs.map(({ name, count, color }) => (
-    <Selector key={name.toLowerCase()} color={color}>
+const Filter = ({ languages, currentLanguage, onClick }) => {
+  const selectors = languages.map(({ name, count, color }) => (
+    <Selector
+      key={name.toLowerCase()}
+      color={color}
+      className={currentLanguage === name ? 'selected' : ''}
+      onClick={() => onClick && onClick(name)}
+    >
       <span>{name}</span>
       <span>{count}</span>
     </Selector>
@@ -19,9 +19,26 @@ const Filter = () => {
   return (
     <Container>
       {selectors}
-      <Cleaner>Limpar</Cleaner>
+      <Cleaner onClick={() => onClick && onClick(undefined)}>Limpar</Cleaner>
     </Container>
   );
+};
+
+Filter.defaultProps = {
+  currentLanguage: null,
+  onClick: null,
+};
+
+Filter.propTypes = {
+  languages: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+      color: PropTypes.string,
+    }).isRequired
+  ).isRequired,
+  currentLanguage: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Filter;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Profile from './Profile';
 import Filter from './Filter';
@@ -6,16 +6,99 @@ import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
-const RepositoriesPage = () => (
-  <Container>
-    <Sidebar>
-      <Profile />
-      <Filter />
-    </Sidebar>
-    <Main>
-      <Repositories />
-    </Main>
-  </Container>
-);
+import { getLangsFrom } from '../../services/api';
+
+const RepositoriesPage = () => {
+  const [currentLanguage, setCurrentLanguage] = useState();
+
+  const user = {
+    login: 'devsamurai',
+    name: 'Felipe',
+    avatar_url: 'https://avatars.githubusercontent.com/u/55540536?v=4',
+    followers: 0,
+    following: 0,
+    company: null,
+    blog: 'https://devsamurai.com.br',
+    location: 'São José dos Campos - SP',
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const repositories = [
+    {
+      id: '1',
+      name: 'Repo 1',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: 'JavaScript',
+    },
+    {
+      id: '2',
+      name: 'Repo 2',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: 'JavaScript',
+    },
+    {
+      id: '3',
+      name: 'Repo 3',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: 'PHP',
+    },
+    {
+      id: '4',
+      name: 'Repo 4',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: null,
+    },
+    {
+      id: '5',
+      name: 'Repo 5',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: 'TypeScript',
+    },
+    {
+      id: '6',
+      name: 'Repo 6',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: 'Ruby',
+    },
+    {
+      id: '7',
+      name: 'Repo 7',
+      description: 'Descrição',
+      html_url: 'https://devsamurai.com.br',
+      language: 'PHP',
+    },
+  ];
+
+  const languages = getLangsFrom(repositories);
+
+  const onFilterClick = (language) => {
+    setCurrentLanguage(language);
+  };
+
+  return (
+    <Container>
+      <Sidebar>
+        <Profile user={user} />
+        <Filter
+          languages={languages}
+          currentLanguage={currentLanguage}
+          onClick={onFilterClick}
+        />
+      </Sidebar>
+      <Main>
+        <Repositories
+          repositories={repositories}
+          currentLanguage={currentLanguage}
+        />
+      </Main>
+    </Container>
+  );
+};
 
 export default RepositoriesPage;
